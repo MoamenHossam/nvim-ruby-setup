044e619e2b672e8d30309527846afccdffe9c895
Moamen Hossam <m.hossam@planradar.com>
2026-01-15 18:53:01 +0200

    add opencode plugin

--- init.lua
+++ init.lua
@@ -139,6 +139,34 @@ vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Half-page up and center cursor
 
 -- custom binds
 vim.keymap.set('n', '<C-a>', 'gg<S-v>G', { desc = 'Select all' })
+
+-- Toggle OpenCode in tmux pane with leader+c
+vim.keymap.set('n', '<leader>c', function()
+  -- Check if we're in tmux
+  if vim.env.TMUX == nil then
+    print 'Not in a tmux session'
+    return
+  end
+
+  -- Check if opencode pane exists by looking for the pane with opencode command
+  local panes = vim.fn.system "tmux list-panes -F '#{pane_id} #{pane_current_command}'"
+  local opencode_pane_id = nil
+
+  for line in panes:gmatch '[^\r\n]+' do
+    if line:match 'opencode' or line:match 'occ' then
+      opencode_pane_id = line:match '^(%%[0-9]+)'
+      break
+    end
+  end
+
+  if opencode_pane_id then
+    -- OpenCode pane exists, just focus it
+    vim.fn.system('tmux select-pane -t ' .. opencode_pane_id)
+  else
+    -- OpenCode pane doesn't exist, create it
+    vim.fn.system 'tmux split-window -h opencode'
+  end
+end, { desc = 'Toggle OpenCode in tmux pane', silent = true })
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
@@ -974,7 +1002,69 @@ require('lazy').setup({
   require 'kickstart.plugins.vimbegood',
   require 'kickstart.plugins.harpoon',
   require 'kickstart.plugins.rspec',
-  require 'kickstart.plugins.copilotchat',
+  -- Replaced copilotchat with opencode.nvim
+  {
+    'NickvanDyke/opencode.nvim',
+    dependencies = {
+      { 'folke/snacks.nvim', opts = { input = {}, picker = {}, terminal = {} } },
+    },
+    config = function()
+      vim.g.opencode_opts = {}
+      vim.o.autoread = true
+
+      vim.keymap.set({ 'n', 'x' }, '<leader>p', function()
+        require('opencode').ask('@this: ', { submit = true })
+      end, { desc = 'Ask opencode' })
+      vim.keymap.set({ 'n', 'x' }, '<C-x>', function()
+        require('opencode').select()
+      end, { desc = 'Execute opencode action…' })
+      -- Smart toggle: focus existing opencode window, or open it if missing
+      local function focus_or_toggle_opencode()
+        local curr = vim.api.nvim_get_current_win()
+        local opencode_win = nil
+        for _, w in ipairs(vim.api.nvim_list_wins()) do
+          local b = vim.api.nvim_win_get_buf(w)
+          local name = vim.api.nvim_buf_get_name(b) or ''
+          local ok, ft = pcall(vim.api.nvim_buf_get_option, b, 'filetype')
+          ft = ok and ft or ''
+          if ft == 'opencode' or name:match 'opencode' then
+            opencode_win = w
+            break
+          end
+        end
+
+        if opencode_win then
+          if curr == opencode_win then
+            vim.cmd 'wincmd p'
+          else
+            vim.api.nvim_set_current_win(opencode_win)
+          end
+        else
+          require('opencode').toggle()
+        end
+      end
+
+      vim.keymap.set({ 'n', 't' }, '<leader>o', focus_or_toggle_opencode, { desc = 'Toggle/focus opencode' })
+      vim.api.nvim_create_user_command('OpencodeToggle', focus_or_toggle_opencode, { desc = 'Toggle/focus opencode' })
+
+      vim.keymap.set({ 'n', 'x' }, 'go', function()
+        return require('opencode').operator '@this '
+      end, { expr = true, desc = 'Add range to opencode' })
+      vim.keymap.set('n', 'goo', function()
+        return require('opencode').operator '@this ' .. '_'
+      end, { expr = true, desc = 'Add line to opencode' })
+
+      vim.keymap.set('n', '<S-C-u>', function()
+        require('opencode').command 'session.half.page.up'
+      end, { desc = 'opencode half page up' })
+      vim.keymap.set('n', '<S-C-d>', function()
+        require('opencode').command 'session.half.page.down'
+      end, { desc = 'opencode half page down' })
+
+      -- avoid remapping + and - to ctrl keys which may be used elsewhere
+      -- increment/decrement left as default behaviour
+    end,
+  },
   require 'kickstart.plugins.session',
   -- require 'kickstart.plugins.glab',
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
